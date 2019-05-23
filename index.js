36a874de4b96476d545845acaec70b0ff8f8e2ac
Anil Kishan <kishan.anil@gmail.com>
2019-05-23 06:06:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-var recipes = {};
+let recipes = {};
 
-function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({},object,{[key]: value})
-}
-
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
-}
-
-function deleteFromObjectByKey(object, key){
-  var newObj =  Object.assign({},object)
-  delete newObj[key]
-  return newObj
-}
-
-function destructivelyDeleteFromObjectByKey(object, key){
-  delete object[key]
-  return object
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({})
 }
